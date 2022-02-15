cd8931ad494ebb0515262b022c43d093f699999a
Marshall <marspatt@ttu.edu>
2022-02-15 17:17:09 -0600

    Added getInstance method, added addUser method

--- src/xyz/marshalldev/User/UserCache.java
+++ src/xyz/marshalldev/User/UserCache.java
@@ -4,9 +4,25 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class UserCache {
-    private Map<Integer, User> users = new HashMap<>();
+    private static UserCache instance;
+    private static Map<Integer, User> users = new HashMap<>();
+
+    public static UserCache getInstance() {
+        if (instance == null) {
+            synchronized (UserCache.class) {
+                if (instance == null) {
+                    instance = new UserCache();
+                }
+            }
+        }
+        return instance;
+    }
 
     public User get(int id) {
         return users.get(id);
+    }
+
+    public void addUser(User user) {
+        users.putIfAbsent(user.getId(), user);
     }
 }
