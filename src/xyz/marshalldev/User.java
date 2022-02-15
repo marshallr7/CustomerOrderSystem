2a30c31d7780354a06bf761666e10345368c3670
Marshall <marspatt@ttu.edu>
2022-02-15 10:03:59 -0600

    Added instance variables and constructor

--- src/xyz/marshalldev/User.java
+++ src/xyz/marshalldev/User.java
@@ -1,4 +1,15 @@
 package xyz.marshalldev;
 
 public class User {
+    private int id;
+    private String password;
+    private String securityQuestion;
+    private String answer;
+
+    public User(int id, String password, String securityQuestion, String answer) {
+        this.id = id;
+        this.password = password;
+        this.securityQuestion = securityQuestion;
+        this.answer = answer;
+    }
 }
