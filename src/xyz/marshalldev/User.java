1d483f61e3281ff552516f9091b5560f89726176
Marshall <marspatt@ttu.edu>
2022-02-15 10:04:20 -0600

    Added getters and setters

--- src/xyz/marshalldev/User.java
+++ src/xyz/marshalldev/User.java
@@ -12,4 +12,36 @@ public class User {
         this.securityQuestion = securityQuestion;
         this.answer = answer;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSecurityQuestion() {
+        return securityQuestion;
+    }
+
+    public void setSecurityQuestion(String securityQuestion) {
+        this.securityQuestion = securityQuestion;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
