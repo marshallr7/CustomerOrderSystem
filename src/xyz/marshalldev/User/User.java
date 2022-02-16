3d64b9c0ec8ee540c724b1c07cb287cdea1946fd
Marshall <marspatt@ttu.edu>
2022-02-16 17:45:26 -0600

    Base user class

---
+++ src/xyz/marshalldev/User/User.java
@@ -0,0 +1,48 @@
+package xyz.marshalldev.User;
+
+public class User {
+
+    private int id;
+    private String password;
+    private String question;
+    private String answer;
+
+    public User(int id, String password, String question, String answer) {
+        this.id = id;
+        this.password = password;
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
