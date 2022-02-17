5198396ed1a1b1b5e8a3e82d5065b17d1372c3ca
Marshall <marspatt@ttu.edu>
2022-02-17 12:36:11 -0600

    Added id and password constructor for testing

--- src/xyz/marshalldev/User/User.java
+++ src/xyz/marshalldev/User/User.java
@@ -17,6 +17,11 @@ public class User {
         this.answer = answer;
     }
 
+    public User(int id, String password) {
+        this.id = id;
+        this.password = password;
+    }
+
     public int getId() {
         return id;
     }
