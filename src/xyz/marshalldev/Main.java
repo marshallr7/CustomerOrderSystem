654cd484cf811c669f8514a67d0a320d5d35c5fd
Marshall <marspatt@ttu.edu>
2022-02-18 18:25:36 -0600

    testing

--- src/xyz/marshalldev/Main.java
+++ src/xyz/marshalldev/Main.java
@@ -1,5 +1,6 @@
 package xyz.marshalldev;
 
+import xyz.marshalldev.User.User;
 import xyz.marshalldev.User.UserManager;
 
 public class Main {
@@ -7,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         UserManager u = new UserManager();
         u.create();
+        User user = u.login();
     }
 }
