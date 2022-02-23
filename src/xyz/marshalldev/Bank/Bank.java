4c8c168f5c2c6629e67902f548c2ecc09bf7cfb7
Marshall <marspatt@ttu.edu>
2022-02-22 18:06:50 -0600

    Created Bank and added storage

---
+++ src/xyz/marshalldev/Bank/Bank.java
@@ -0,0 +1,11 @@
+package xyz.marshalldev.Bank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Bank {
+
+    private Map<Long, Account> accounts = new HashMap<>();      // Storage for user fund information. <Credit Card Number, Account>
+
+
+}
