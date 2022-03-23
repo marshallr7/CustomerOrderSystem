7eb6013f63392cf5296d81b399d4b9dc2615482c
Marshall <marspatt@ttu.edu>
2022-03-22 20:42:07 -0500

    Completed Order class

--- src/xyz/marshalldev/Order.java
+++ src/xyz/marshalldev/Order.java
@@ -2,13 +2,50 @@ package xyz.marshalldev;
 
 import xyz.marshalldev.Item.Item;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Order {
 
-    private static final HashMap<Integer, Item> itemsOrdered = new HashMap<>();
+    private int orderNumber;
+    private Cart cart;
+    private Date date;
+
+    public Order(Cart cart, int orderNumber) {
+        this.cart = cart;
+        this.orderNumber = orderNumber;
+
+        date = new Date();
+    }
+
+    public int getOrderNumber() {
+        return orderNumber;
+    }
+
+    public void setOrderNumber(int orderNumber) {
+        this.orderNumber = orderNumber;
+    }
+
+    public Cart getCart() {
+        return cart;
+    }
+
+    public void setCart(Cart cart) {
+        this.cart = cart;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 
     //The system stores the customer order, containing the order date, customer ID, product name,
     //quantity, total, and authorization number.
     //9. The system displays order confirmation to the customer.
+
 }
