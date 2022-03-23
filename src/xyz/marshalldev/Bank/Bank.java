a3c12764729ef89b504132626bd8504314a9ed68
Marshall <marspatt@ttu.edu>
2022-03-22 20:40:18 -0500

    generateConfirmation number method

--- src/xyz/marshalldev/Bank/Bank.java
+++ src/xyz/marshalldev/Bank/Bank.java
@@ -2,10 +2,11 @@ package xyz.marshalldev.Bank;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class Bank {
 
-    private static Map<Long, Account> accounts = new HashMap<>();      // Storage for user fund information. <Credit Card Number, Account>
+    private static final Map<Long, Account> accounts = new HashMap<>();      // Storage for user fund information. <Credit Card Number, Account>
 
     public static void addAccount(long creditCardNumber, Account account) {
         accounts.putIfAbsent(creditCardNumber, account);
@@ -17,6 +18,11 @@ public class Bank {
 
     public static Account getAccount(long creditCardNumber) {
         return accounts.get(creditCardNumber);
+    }
+
+    public static int generateConfirmation() {
+        Random rand = new Random();
+        return rand.nextInt(1000, 10000);
     }
 
 }
