e76e157fdd6b0d3e09b683c4f49d2bf563f314d9
Marshall <marspatt@ttu.edu>
2022-03-22 20:41:55 -0500

    Completed Main class

--- src/xyz/marshalldev/Main.java
+++ src/xyz/marshalldev/Main.java
@@ -1,7 +1,5 @@
 package xyz.marshalldev;
 
-import xyz.marshalldev.Bank.Account;
-import xyz.marshalldev.Bank.Bank;
 import xyz.marshalldev.Item.Item;
 import xyz.marshalldev.Item.ItemManager;
 import xyz.marshalldev.User.User;
@@ -10,16 +8,11 @@ import xyz.marshalldev.User.UserManager;
 public class Main {
 
     private static User user = null;
+    public static final double STARTING_BANK_BALANCE = 15000d;
 
     public static void main(String[] args) {
-        UserManager u = new UserManager();
-//        u.create();
-//        user = u.login();
-        createItems();
-        User user1 = new User(1234, "Test@1", "Hello?", "Yes", "Marshall", "17 Westhaven", 123124);
-        Bank.addAccount(123124, new Account(15000));
-        u.addUser(user1);
-        setUser(user1);
+        createTestItems();
+        createTestUsers();
         Impl.driver();
     }
 
@@ -31,11 +24,22 @@ public class Main {
         Main.user = user;
     }
 
-    private static void createItems() {
-        Item item = new Item("Test", "This is a test item", 12.01, 9.99, false, 100);
-        Item item2 = new Item("Test2", "This is a test item", 100.00, 99.99, true, 20);
-        ItemManager.getInstance().addItems(item, item2);
 
+    private static void createTestItems() {
+        Item xbox = new Item("Xbox", "Xbox series X", 600, 550, false);
+        Item playstation = new Item("Playstation", "Playstation 5", 900, 750, true);
+        Item pc = new Item("PC", "Some pc", 3000.00, 2700, false);
+        Item calculator = new Item("Calculator", "TI Inspire", 100.00, 99.99, false);
+        Item monitor = new Item("Monitor", "some monitor", 200.00, 150.00, false);
+        ItemManager.getInstance().addItems(xbox, playstation, pc, calculator, monitor);
+    }
 
+    private static void createTestUsers() {
+        User user1 = new User(123, "Test@1", "Test Question", "Answer", "Pablo", "123 test street", 123123);
+        User user2 = new User(1234, "Test@1", "Test Question", "Answer", "Not Pablo", "123 street", 12312323);
+        User user3 = new User(12345, "Test@1", "Test Question", "Answer", "Who?", "123 t", 12391123);
+        User user4 = new User(122, "Test@1", "Test Question", "Answer", "Juan", "1 test street", 12);
+        User user5 = new User(126, "Test@1", "Test Question", "Answer", "Umm", "No", 1415);
+        UserManager.getInstance().addUsers(user1, user2, user3, user4, user5);
     }
 }
